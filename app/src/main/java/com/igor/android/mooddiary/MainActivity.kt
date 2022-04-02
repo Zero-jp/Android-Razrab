943b974f5c9f52cd4d11c7d3c1db047a7b351726
Igor Kochugin <zero-jpg@inbox.ru>
2022-04-03 00:01:43 +0300

    Main page and note view page are added/

---
+++ app/src/main/java/com/igor/android/mooddiary/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.igor.android.mooddiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
